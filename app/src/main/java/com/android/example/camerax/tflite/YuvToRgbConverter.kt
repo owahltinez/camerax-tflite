52708fe71fe7d1eaf830ccd400d84a4aacb807e6
Oscar Wahltinez <oscar@wahltinez.org>
2020-04-02 21:16:16 -0400

    Pass reference of intermediate output buffer

--- app/src/main/java/com/android/example/camerax/tflite/YuvToRgbConverter.kt
+++ app/src/main/java/com/android/example/camerax/tflite/YuvToRgbConverter.kt
@@ -40,7 +40,7 @@ class YuvToRgbConverter(context: Context) {
     private val scriptYuvToRgb = ScriptIntrinsicYuvToRGB.create(rs, Element.U8_4(rs))
 
     private var pixelCount: Int = -1
-    private lateinit var outputBuffer: ByteBuffer
+    private lateinit var yuvBuffer: ByteBuffer
     private lateinit var inputAllocation: Allocation
     private lateinit var outputAllocation: Allocation
 
@@ -48,31 +48,31 @@ class YuvToRgbConverter(context: Context) {
     fun yuvToRgb(image: Image, output: Bitmap) {
 
         // Ensure that the intermediate output byte buffer is allocated
-        if (!::outputBuffer.isInitialized) {
+        if (!::yuvBuffer.isInitialized) {
             pixelCount = image.cropRect.width() * image.cropRect.height()
-            outputBuffer = ByteBuffer.allocateDirect(
+            yuvBuffer = ByteBuffer.allocateDirect(
                 pixelCount * ImageFormat.getBitsPerPixel(ImageFormat.YUV_420_888) / 8)
         }
 
         // Get the YUV data in byte array form
-        val yuvBytes = imageToByteBuffer(image)
+        imageToByteBuffer(image, yuvBuffer)
 
         // Ensure that the RenderScript inputs and outputs are allocated
         if (!::inputAllocation.isInitialized) {
-            inputAllocation = Allocation.createSized(rs, Element.U8(rs), yuvBytes.array().size)
+            inputAllocation = Allocation.createSized(rs, Element.U8(rs), yuvBuffer.array().size)
         }
         if (!::outputAllocation.isInitialized) {
             outputAllocation = Allocation.createFromBitmap(rs, output)
         }
 
         // Convert YUV to RGB
-        inputAllocation.copyFrom(yuvBytes.array())
+        inputAllocation.copyFrom(yuvBuffer.array())
         scriptYuvToRgb.setInput(inputAllocation)
         scriptYuvToRgb.forEach(outputAllocation)
         outputAllocation.copyTo(output)
     }
 
-    private fun imageToByteBuffer(image: Image): ByteBuffer {
+    private fun imageToByteBuffer(image: Image, outputBuffer: ByteBuffer) {
         assert(image.format == ImageFormat.YUV_420_888)
 
         val imageCrop = image.cropRect
@@ -171,6 +171,5 @@ class YuvToRgbConverter(context: Context) {
                 }
             }
         }
-        return outputBuffer
     }
 }
